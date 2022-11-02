f96b8d909bfbf37f2c4971b2129460a271f532e9
lRadixl <114234474+lRadixl@users.noreply.github.com>
2022-11-02 22:23:49 +0300

    Delete TaskContentBinding directory

--- TaskContentBinding/TaskContentBinding 1.js
+++
@@ -1,8 +0,0 @@
-var user = {
-    name: "Tom",
-};
-function format(beginMsg, endMsg) {
-    console.log(beginMsg + this.name + endMsg);
-}
-var tomFormat = format.bind(user)
-tomFormat("<<<", ">>>");
